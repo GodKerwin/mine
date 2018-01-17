2f18416344e4188cbfbcb4b6c8b33a8a4d60c084
lxu <lxu@landhightech.com>
2018-01-17 16:57:01 +0800

    创建电商分支

--- src/main/resources/webapp/app/js/main.js
+++ src/main/resources/webapp/app/js/main.js
@@ -48,12 +48,19 @@ App.controller('myCtrl', function ($scope, $rootScope, $location, $window, $cook
 
 //关于控制器
 App.controller('aboutCtrl', function ($scope, $location, mine) {
-    console.log("this is about controller")
+    console.log("this is about controller");
 });
 
 //主界面控制器
 App.controller('homeCtrl', function ($scope, $location, mine) {
-    console.log("this is home controller")
+    console.log("this is home controller");
+    $("#img-carousel").carousel('cycle');
+    $scope.prev = function () {
+        $("#img-carousel").carousel('prev');
+    }
+    $scope.next = function () {
+        $("#img-carousel").carousel('next');
+    }
 });
 
 //=========本地存储数据服务============
